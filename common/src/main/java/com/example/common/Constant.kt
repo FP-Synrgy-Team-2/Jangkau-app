4d4bf8c436ae03e3d5c4809d5361e5ba356776e3
GalangNour <galangnour4@gmail.com>
2024-08-14 13:30:57 +0700

    rebase api url

--- common/src/main/java/com/example/common/Constant.kt
+++ common/src/main/java/com/example/common/Constant.kt
@@ -1,7 +1,7 @@
 package com.example.common
 
 object Constant {
-    const val BASE_URL = "https://practical-winnifred-jangkaubybca-00a04785.koyeb.app/"
+    const val BASE_URL = "https://jangkau-syn7.duckdns.org/api/"
 
 
 
